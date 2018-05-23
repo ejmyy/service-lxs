cdbc1aac36f31def2163475189ce3f6777cd72a6
hrdyjan1 <hrdyjan1@gmail.com>
2018-05-23 21:33:44 +0200

    Toggle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Grid } from 'react-bootstrap';
+import Toggle from 'react-bootstrap-toggle';
 import { 
   BrowserRouter as Router,
   Route,
@@ -16,36 +17,72 @@ import Happy from './components/pages/happy';
 import Login from './components/pages/login';
 import Footer from './components/footer/footer';
 
+
 // Includes
 import './assets/css/main.css';
 import CarServis from './components/pages/carservis';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = { toggleActive: true };
+    this.onToggle = this.onToggle.bind(this);
+  }
+ 
+  onToggle() {
+    this.setState({ toggleActive: !this.state.toggleActive });
+  }
+
+  renderSwitch(isActive) {
+
+    if(isActive) {
+        return <Router>
+                  <div className="App">
+
+                   <Header/>
+
+                     <Grid>
+
+                       <Route exact path='/' component={HomePage} />
+                       <Route exact path='/servis' component={Servis} />
+                       <Route exact path='/carservis' component={CarServis}/>
+                       <Route exact path='/carrepair' component={CarRepair}/>
+                       <Route exact path='/faq' component={Default} />
+                       <Route exact path='/happy' component={Happy} />
+                       <Route exact path='/login' component={Login} />
+
+                     </Grid>      
+
+                   <Footer/>  
+
+                 </div>
+                </Router>
+      } else {
+        return 'foo';
+      }
+  }
+  
+
+
   render() {
 
     return (
-      <Router>
-        <div className="App">
-
-            <Header/>
-
-              <Grid>
-
-                <Route exact path='/' component={HomePage} />
-                <Route exact path='/servis' component={Servis} />
-                <Route exact path='/carservis' component={CarServis}/>
-                <Route exact path='/carrepair' component={CarRepair}/>
-                <Route exact path='/faq' component={Default} />
-                <Route exact path='/happy' component={Happy} />
-                <Route exact path='/login' component={Login} />
-
-              </Grid>      
-              
-            
-            <Footer/>        
-        
-        </div>
-      </Router>
+        <div>
+          <div>
+            {this.renderSwitch(this.state.toggleActive)}
+          </div>
+    
+        <form>
+          <Toggle className="toggleButton"
+            onClick={this.onToggle}
+            on="Klient"
+            off="Servis"
+            offstyle="danger"
+            active={this.state.toggleActive}
+          />
+        </form>
+      </div>
     );
   }
 }
