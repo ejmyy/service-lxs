6f2ccb27015babfc5fa606a5a7b320159947c8c1
hrdyjan1 <hrdyjan1@gmail.com>
2018-05-24 09:02:09 +0200

    non existing URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { Grid } from 'react-bootstrap';
 import Toggle from 'react-bootstrap-toggle';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom';
 
 // Components Klient
 import Header from './components/header/header';
@@ -51,13 +51,16 @@ class App extends Component {
       return (
         <Grid>
 
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/servis' component={Servis} />
-          <Route exact path='/carservis' component={CarServis}/>
-          <Route exact path='/carrepair' component={CarRepair}/>
-          <Route exact path='/default' component={Default} />
-          <Route exact path='/happy' component={Happy} />
-          <Route exact path='/login' component={Login} />
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route exact path='/servis' component={Servis} />
+            <Route exact path='/carservis' component={CarServis}/>
+            <Route exact path='/carrepair' component={CarRepair}/>
+            <Route exact path='/default' component={Default} />
+            <Route exact path='/happy' component={Happy} />
+            <Route exact path='/login' component={Login} />
+            <Redirect to="/" />
+          </Switch>
           
         </Grid>
       );
@@ -65,13 +68,16 @@ class App extends Component {
       return (
         <Grid>
 
-          <Route exact path='/' component={HomePageServis} />
-          <Route exact path='/myPage' component={MyPage} />
-          <Route exact path='/carservisservis' component={CarServiceDetail} />
-          <Route exact path='/requests' component={Request} />
-          <Route exact path='/loginJob' component={LoginJob} />
-          <Route exact path='/personalProfile' component={HomePageServis} />
-
+          <Switch>
+            <Route exact path='/' component={HomePageServis} />
+            <Route exact path='/myPage' component={MyPage} />
+            <Route exact path='/carservisservis' component={CarServiceDetail} />
+            <Route exact path='/requests' component={Request} />
+            <Route exact path='/loginJob' component={LoginJob} />
+            <Route exact path='/personalProfile' component={HomePageServis} />
+            <Redirect to="/" />
+          </Switch>
+          
       </Grid>
       );      
     }
